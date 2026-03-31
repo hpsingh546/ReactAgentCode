ea600efa48a8e7c8b6beac154b60512ec6e76489
hpsingh546 <harmanreetsingh546@gmail.com>
2026-04-01 00:04:55 +0530

    custom tool calling

--- agent.js
+++ agent.js
@@ -1,27 +1,53 @@
 import { ChatGroq } from "@langchain/groq";
-import { createAgent } from "langchain";
+import { createAgent, tool } from "langchain";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import "dotenv/config";
 import { TavilySearch } from "@langchain/tavily";
+import z from "zod";
 
-const Search = new TavilySearch({
-  maxResults: 5,
-  topic: "general",
-});
 async function main() {
+  const Search = new TavilySearch({
+    maxResults: 5,
+    topic: "general",
+  });
+  const calendarEvents = tool(
+    async ({ query }) => {
+      // Google calendar logic goes
+      return JSON.stringify([
+        {
+          title: "Meeting with Sujoy",
+          date: "9th Aug 2025",
+          time: "2 PM",
+          location: "Gmeet",
+        },
+      ]);
+    },
+    {
+      name: "get-calendar-events",
+      description: "Call to get the calendar events.",
+      schema: z.object({
+        query: z
+          .string()
+          .describe("The query to use in calendar event search."),
+      }),
+    },
+  );
   const model = new ChatGroq({
     model: "openai/gpt-oss-120b",
     temperature: 0,
-    maxTokens: undefined,
-    maxRetries: 2,
-    // other params...
   });
   const agent = createAgent({
     model: model,
-    tools: [Search],
+    tools: [Search, calendarEvents],
   });
   const result = await agent.invoke({
-    messages: [{ role: "user", content: "current dow future value" }],
+    messages: [
+      {
+        role: "system",
+        content: `You are a personal assistant. Use provided tools to get the information if you don't have it. Current date and time: ${new Date().toUTCString()}`,
+      },
+      { role: "user", content: "iran news today" },
+    ],
   });
   console.log(result.messages[result.messages.length - 1].content);
 }
